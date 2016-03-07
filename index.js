f931e77eb69b297ba72f6b4061ecf325c8fda636
kreo <hi@kreo.me>
2016-03-07 02:18:09 +0100

    added series

--- index.js
+++ index.js
@@ -6,51 +6,30 @@
 //  http://opensource.org/licenses/MIT
 // ---------------------------------------------
 
-var nanoshell = (function () {
+var nanoshell = (function() {
 
-    var child_process = require('child_process'),
-        colors = require('chalk'),
-        config = require('./lib/config'),
-        tasks = require('./lib/tasks');
-
-    function printTasks (tasks) {
-        console.log(colors.gray(''));
-        console.log(colors.gray('Tasks'));
-        console.log(colors.gray('-------------------'));
-
-        for (var i = 0; i < Object.keys(tasks).length; i++) {
-            var task = Object.keys(tasks)[i];
-            console.log(colors.cyan(task));
-        }
-    }
-
-    function exec(cmd, cb) {
-        var parts = cmd.split(/\s+/g);
-        var spawn = child_process.spawn(parts[0], parts.slice(1), { stdio: 'inherit' });
-        spawn.on('exit', function(code) {
-            var err = null;
-            if (code) {
-                err = new Error('command "' + cmd + '" exited with wrong status code "' + code + '"');
-                err.code = code;
-                err.cmd = cmd;
-            }
-            if (cb) cb(err);
-        });
-    }
+    var shell = require('./lib/shell');
+    var config = require('./lib/config');
+    var tasks = require('./lib/tasks');
 
     function run(argv) {
         argv.forEach(function(val, index) {
+            var callstack = [];
             var task = tasks[val];
-            if (task !== undefined){
-                exec(task());
+            if (task !== undefined) {
+                callstack.push(task());
+                shell.series(callstack, function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            } else if (val === 'list') {
+                shell.printTasks(tasks);
             }
-            else if (val === 'list'){
-                printTasks(tasks);
-            }
-        });
+        })
     }
 
-    var init = function() {
+    function init() {
         run(process.argv);
     }
 
